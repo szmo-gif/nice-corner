6261fd9f1c516ad692b4fd5f396e4d2067d5f065
Szmoniewski Baptiste <151198403+szmo-gif@users.noreply.github.com>
2024-08-12 11:50:23 +0200

    sécuriser le fichier firebase

--- firebase.js
+++ firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBD3RMDc3NWNfse2jS6Lk8eWs3ax6Y3t2E",
-  authDomain: "nice-corner.firebaseapp.com",
-  projectId: "nice-corner",
-  storageBucket: "nice-corner.appspot.com",
-  messagingSenderId: "752941395606",
-  appId: "1:752941395606:web:adbffb1e452bdbac675cb7",
-  measurementId: "G-W5SEE3DYR5"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
